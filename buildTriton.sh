106772082a4f6ecdf345aae0b7124e8bae06352f
Alexander Weinrauch <alexander.weinrauch@amd.com>
2024-10-03 14:20:04 +0000

    Initial scripts

---
+++ buildTriton.sh
@@ -0,0 +1,17 @@
+if ! command -v ccache 2>&1 > /dev/null
+then
+    sudo apt -y install ccache
+fi
+if ! command -v tmux 2>&1 > /dev/null
+then
+    sudo apt -y install tmux
+fi
+if ! python -c "import pybind11" &> /dev/null; then
+    pip install pybind11
+fi
+
+export REL_WITH_DEB_INFO=true
+export TRITON_BUILD_WITH_CLANG_LLD=true
+export TRITON_BUILD_WITH_CCACHE=true
+
+pip install -e python
