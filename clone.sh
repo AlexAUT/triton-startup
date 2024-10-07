7a49be820e573e1c22cd29c84132f7fe30873c1a
Alexander Weinrauch <alexander.weinrauch@amd.com>
2024-10-07 12:09:20 +0000

    Symlink to scripts from clonded repo

--- clone.sh
+++ clone.sh
@@ -2,3 +2,5 @@ git clone https://github.com/alexaut/triton
 cd triton
 git remote add upstream https://github.com/triton-lang/triton
 git remote add rocm https://github.com/rocm/triton
+ln -sf ../triton-startup scripts
+ln -sf ../triton-startup/.vscode .vscode
