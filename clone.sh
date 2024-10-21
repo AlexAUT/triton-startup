a74ccd902468e792ccbc5a9e8e31491f4be98005
Alexander Weinrauch <alexander.weinrauch@amd.com>
2024-10-21 10:02:13 +0000

    Made cloning more rebust and add gdb pretty printer setup

--- clone.sh
+++ clone.sh
@@ -1,6 +1,14 @@
+#/bin/bash
+
+script_dir="$(cd "$(dirname -- "$0")" && pwd)"
+
 git clone https://github.com/alexaut/triton
 cd triton
 git remote add upstream https://github.com/triton-lang/triton
 git remote add rocm https://github.com/rocm/triton
-ln -sf ../triton-startup scripts
-ln -sf ../triton-startup/.vscode .vscode
+ln -sf ${script_dir} scripts
+ln -sf ${script_dir}/.vscode .vscode
+
+# Generate gdbinit
+echo "source ${script_dir}/llvm-pretty-printers.py" >> .gdbinit
+echo "source ${script_dir}/llvm-ir-pp/llvm-ir-prettyprinter.py" >> .gdbinit
