10762f1f1e6dea4b5d3b5a3f6742a3aab733b632
Alexander Weinrauch <alexander.weinrauch@amd.com>
2025-07-17 08:09:31 +0000

    Add default env

---
+++ defaultEnv.sh
@@ -0,0 +1,12 @@
+export TRITON_ALWAYS_COMPILE=1
+export MLIR_DUMP_PATH=build-out/out.mlir
+export TRITON_KERNEL_DUMP=1
+export TRITON_OVERRIDE_DIR=build-out/override/
+export TRITON_DUMP_DIR=build-out/dump/
+export TRITON_LLVM_DEBUG_ONLY=""
+export TRITON_REPRODUCER_PATH=build-out/reproducer.mlir
+
+# export LLVM_BUILD_DIR=/home/aweinrau/llvm-project/build
+# export LLVM_INCLUDE_DIRS=$LLVM_BUILD_DIR/include
+# export LLVM_LIBRARY_DIR=$LLVM_BUILD_DIR/lib
+# export LLVM_SYSPATH=$LLVM_BUILD_DIR 
